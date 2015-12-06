23c76b55d4aef5851b68a3bd770b838f110c5106
Denis Pavlyutchenko <Hixon10@yandex.ru>
2015-12-06 05:52:23 +0300

    Frontend is done

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -33,7 +33,48 @@ function init() {
     });
 }
 
+function insertPosts(posts) {
+    var postsHtml = "";
+
+    for (var i = 0; i < posts.length; i++) {
+        postsHtml += '<div class="blog-post">';
+        postsHtml += '<h2 class="blog-post-title">' + posts[i].title + '</h2>';
+        postsHtml += '<p class="blog-post-meta">' + posts[i].createdDate.month + ' ' + posts[i].createdDate.dayOfMonth + ', ' + posts[i].createdDate.year + ' by <a href="#">Chris</a></p>';
+        postsHtml += '<div>' + posts[i].content + '</div>';
+        postsHtml += '</div>';
+    }
+
+    $("#contentContainer").html(postsHtml);
+}
+
+function insertAllCategories() {
+    var categoriesHtml = "";
+
+    for (var i = 0; i < categories.length; i++) {
+        categoriesHtml += '<li><a data-category_title="' + categories[i].title + '" class="categoryLink" href="#">' + categories[i].title + '</a></li>';
+    }
+
+    $('#categoryList').html(categoriesHtml);
+}
+
 $(document).ready(function() {
     init();
+    insertPosts(posts);
+    insertAllCategories();
 
+    $("#homePageLink").click(function(){
+        insertPosts(posts);
+    });
+
+    $('body').on('click', 'a.categoryLink', function() {
+        var $el = $(this);
+        var categoryTitle = $el.data('category_title');
+
+        for (var i = 0; i < categories.length; i++) {
+            if (categories[i].title == categoryTitle) {
+                insertPosts(categories[i].posts);
+                break;
+            }
+        }
+    });
 });
